09687d80fde5caaa2071ca5ad1d4911569200832
jillur984 <jillur.cse.bd@gmail.com>
2025-12-27 12:37:27 +0600

    login validate

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,28 +1,85 @@
 import Container from '@/components/Container'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { cn } from '../components/ui/cn'
 import Title from '@/components/Title'
 import Input, { Label } from '@/components/Input'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Signup = ({className}) => {
+
+
+  // initial state
+
+  const[clientName,setClientName]=useState('')
+  const[email,setEmail]=useState('')
+  const[password,setPassword]=useState('')
+  const[checked,setChecked]=useState(false)
+
+  // error state
+
+  const[errorClientName,setErrorClientName]=useState('')
+  const[errorEmail,setErrorEmail]=useState('')
+  const[errorPassword,setErrorPassword]=useState('')
+
+  const navigate=useNavigate();
+
+  const token=localStorage.getItem('token')
+
+  // console.log("token",token)
+
+  useEffect(()=>{
+      if(token) navigate('/')
+  },[token])
+
+
+  const handleName=(e)=>{
+   setClientName(e.target.value)
+   setErrorClientName('')
+  }
+
+  const handleEmail=(e)=>{
+    setEmail(e.target.value)
+    setErrorEmail('')
+  }
+
+  const handlePassword=(e)=>{
+     setPassword(e.target.value)
+     setErrorPassword("")
+  }
+
+  
+  const handleSignUp=async(e)=>{
+     e.preventDefault()
+     if(!clientName){
+      setErrorClientName("Enter your Name")
+     }
+  }
+  
+
   return (
     <Container>
       <Title className='text-center mb-10'>Create your Account</Title>
        <div className='flex flex-col gap-4 items-center justify-center'>
        <div className='flex gap-2 items-center'>
           <Label htmlFor="Name" className="font-normal">Full Name</Label>
-          <Input placeholder="Enter your Email" type="text"/>
+          <Input placeholder="Enter your Name" type="text" value={clientName} onChange={handleName}/>
         </div>
+        {
+          errorClientName && <p className='text-red'>{errorClientName}</p>
+        }
          <div className='flex gap-2 items-center'>
           <Label htmlFor="email" className="font-normal">Work Email</Label>
-          <Input placeholder="Enter your Email" type="email"/>
+          <Input placeholder="Enter your Email" type="email" value={email} onChange={handleEmail}/>
         </div>
         <div className='flex gap-2 items-center '>
           <Label htmlFor="password" className="font-normal">Work Password</Label>
-          <Input className="border" placeholder="Enter your Password" type="password"/>
+          <Input className="border" placeholder="Enter your Password" type="password" value={password} onChange={handlePassword}/>
         </div>
-        <button className='bg-slate-400 max-w-80 w-full p-2'>Create your account</button>
+        <div className='flex items-center gap-2'>
+          <input type='checkbox'/>
+          <p>I agree to Items of service and privacy ploicy</p>
+        </div>
+        <button onClick={handleSignUp} className='bg-slate-400 max-w-80 w-full p-2'>Create your account</button>
         <p>Already Have Account{""} <Link to="/signin"><span className='text-green-500'>Sign In</span></Link></p>
        </div>
     </Container>
